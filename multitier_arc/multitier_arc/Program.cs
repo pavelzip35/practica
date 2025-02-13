6d563b870ef3ac26cdb6024fda9b7d588bb6eed6
Shrek <pavelalekskat@gmail.com>
2025-02-13 22:19:29 +0300

    afasfsaf

---
+++ multitier_arc/multitier_arc/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Prog
+{
+    class User
+    {
+        public Guid id;
+        public string Nickname;
+    }
+
+
+
+    class Program
+    {
+
+        static User GetUser()
+        {
+            User user = new User();
+
+            user.id = Guid.NewGuid();
+
+            while (true)
+            {
+
+                user.Nickname = "@" + Console.ReadLine();
+                if (!user.Nickname.Contains(' '))
+                {
+                    break;
+                }
+
+
+            }
+
+            return user;
+
+        }
+
+        /*static void adding(string[] arg,)
+        {
+            args.Add();
+        }*/
+
+        static void Main(string[] args)
+        {
+            var Iuser = GetUser();
+
+            private List<string> users = new List<string>() { };
+            users.Add(Iuser);
+        }
+
+            
+    }
+}
+
+
+
+
